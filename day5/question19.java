7b8ad3ce0354a82f7dd132e2ddd03c5c40357e0e
moreofyash <25112cn077@glbitm.ac.in>
2026-06-10 11:22:43 +0530

    Create question19.java

---
+++ day5/question19.java
@@ -0,0 +1,20 @@
+// write a program to print factors of a number
+
+package day5;
+import java.util.Scanner;
+
+public class question19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+        int n = sc.nextInt();
+        System.out.print("Factors are : ");
+        for(int i = 1; i <= n/2; i++){
+            
+            if(n % i == 0){
+                System.out.print(i+" ");
+            }
+        }
+        sc.close();
+    }
+}
