ad56b8fe33b5af193e5de3cfc665f4dc5129933a
moreofyash <25112cn077@glbitm.ac.in>
2026-06-10 11:55:41 +0530

    .

---
+++ day9/question34.java
@@ -0,0 +1,19 @@
+// write a program to print reverse number triangle 
+
+package day9;
+import java.util.Scanner;
+
+public class question34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows of triangle : ");
+        int n = sc.nextInt();
+        for (int i = n; i >= 1; i--) {
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
