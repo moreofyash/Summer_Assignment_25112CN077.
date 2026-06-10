ea7ed6031273d9bc0ec2d8ca2d7bce3947f6cca7
moreofyash <25112cn077@glbitm.ac.in>
2026-06-10 11:58:49 +0530

    .

---
+++ day9/question36.java
@@ -0,0 +1,23 @@
+// write a program to print hollow square pattern 
+
+package day9;
+import java.util.Scanner;
+
+public class question36 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length of side of square : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++){
+                if(i == 1 || i == n || j == 1 || j == n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
