b318ccdc5ca632b21548d47af1e515a298c6205d
yash singh <25112cn077@glbitm.ac.in>
2026-06-20 10:34:06 +0530

    .

---
+++ day:19/question74.java
@@ -0,0 +1,48 @@
+// write a program to subtract matrices 
+
+package day19;
+import java.util.Scanner;
+
+public class question74 {
+    
+    public static int[][] subMatrices(int matrices1[][] , int matrices2[][] , int n , int m){
+        int sub[][] = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                sub[i][j] = matrices1[i][j] - matrices2[i][j];
+            }
+        }
+        return sub;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of Rows : "); 
+        int n = sc.nextInt();
+        System.out.print("Enter number of Columns : "); 
+        int m = sc.nextInt();
+
+        int matrices1[][] = new int[n][m];
+        int matrices2[][] = new int[n][m];
+        System.out.print("Enter elements of first matrices : ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrices1[i][j] = sc.nextInt();
+            }
+        }
+        System.out.print("Enter elements of second matrices : ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrices2[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Subtraction of Matrices : ");
+        int result[][] = subMatrices(matrices1, matrices2, n, m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
