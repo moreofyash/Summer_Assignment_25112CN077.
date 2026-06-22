ec075a66f75ea868067b9ad1305fb2967dd2518f
yash singh <25112cn077@glbitm.ac.in>
2026-06-22 22:15:08 +0530

    .

---
+++ day:21/question83.java
@@ -0,0 +1,33 @@
+// write a program to count vowels and consonants
+
+package day21;
+import java.util.Scanner;
+
+public class question83 {
+    public static void countVowelsAndConsonants(String str) {
+        int vowels = 0;
+        int consonants = 0;
+        str = str.toLowerCase();
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch >= 'a' && ch <= 'z') {
+                if(ch == 'a' || ch == 'e' || ch == 'i' ||
+                    ch == 'o' || ch == 'u') {
+                    vowels++;
+                }else {
+                    consonants++;
+                }
+            }
+        }
+        System.out.println("Vowels = " + vowels);
+        System.out.println("Consonants = " + consonants);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        countVowelsAndConsonants(str);
+        sc.close();
+    }
+}
